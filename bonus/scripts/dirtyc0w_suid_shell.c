b61456522178efea3d7925d1492872084a338c40
Nathanloupy <94622983+Nathanloupy@users.noreply.github.com>
2025-10-21 13:15:54 +0200

    fix: instructions in the comments

--- bonus/scripts/dirtyc0w_suid_shell.c
+++ bonus/scripts/dirtyc0w_suid_shell.c
@@ -1,28 +1,15 @@
 //
 // This exploit uses the pokemon exploit of the dirtycow vulnerability
-// as a base and automatically generates a new passwd line.
-// The user will be prompted for the new password when the binary is run.
-// The original /etc/passwd file is then backed up to /tmp/passwd.bak
-// and overwrites the root account with the generated line.
-// After running the exploit you should be able to login with the newly
-// created user.
-//
-// To use this exploit modify the user values according to your needs.
-//   The default is "toor".
+// as a base and automatically modifies the targeted file..
 //
 // Original exploit (dirtycow's ptrace_pokedata "pokemon" method):
 //   https://github.com/dirtycow/dirtycow.github.io/blob/master/pokemon.c
 //
 // Compile with:
-//   gcc -pthread dirty.c -o dirty -lcrypt
+//   gcc -pthread dirtyc0w_suid_shell.c -o dirtyc0w_suid_shell -lcrypt
 //
 // Then run the newly create binary by either doing:
-//   "./dirty" or "./dirty my-new-password"
-//
-// Afterwards, you can either "su toor" or "ssh toor@..."
-//
-// DON'T FORGET TO RESTORE YOUR /etc/passwd AFTER RUNNING THE EXPLOIT!
-//   mv /tmp/passwd.bak /etc/passwd
+//   "./dirtyc0w_suid_shell"
 //
 // Exploit adopted by Christian "firefart" Mehlmauer
 // https://firefart.at
